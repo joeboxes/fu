c206be8610d85e64ab27d6a728aebeeba683be61
Richard Zirbes <richard.zirbes@magnopus.com>
2021-12-14 18:05:02 -0800

    yup

--- projects/crossword/Unity/Cross/Assets/Scripts/Application/PuzzleGroupsScreenView.cs
+++ projects/crossword/Unity/Cross/Assets/Scripts/Application/PuzzleGroupsScreenView.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PuzzleGroupsScreenView : MonoBehaviour
 {
     public static string EVENT_SELECT_PUZZLE = "PuzzleGroupsScreenView.EVENT_SELECT_PUZZLE";
+    
+    public GameObject _rowPrefab;
+    public GameObject _scrollContainer;
+
+    public ScrollRect _scrollView;
     
     // button hookups
     public void OnPuzzleSelect()
     {
         Debug.Log("OnPuzzleSelect");
         Dispatch.Instance.AlertAll(LandingScreenView.EVENT_SELECT_PUZZLES, this);
+        // GetComponent<ScrollRect>();
     }
     
     // Start is called before the first frame update
@@ -18,11 +25,39 @@ public class PuzzleGroupsScreenView : MonoBehaviour
     {
         var client = PuzzleRemoteClient.Instance;
         client.RequestPuzzleGroups(0, 10);
+        Debug.Log("RICHIE - start groups");
+    
+        for(int i=0; i<10; ++i)
+        {
+            var row = GameObject.Instantiate(_rowPrefab);
+            row.transform.SetParent(_scrollContainer.transform);
+            var script = row.GetComponent<PuzzleGroupRow>();
+            script.row = i;
+            script.OnRowSelectEvent += RowSelectEventHandler;
+        }
+        
+        //var scrollView = GetComponent<ScrollRect>();
+        // scrollView.verticalNormalizedPosition = 0.0f;
+        //scrollView.normalizedPosition = new Vector2(0,1);
+        _scrollView.normalizedPosition = new Vector2(0,1);
     }
+
+    private void RowSelectEventHandler(PuzzleGroupRow row)
+    {
+        Debug.Log("ScreenView - RowSelectEventHandler: "+row.row);
+        
+        
+        // for the moment -> go to the puzzle view
+        
+        
+    } 
 
     // Update is called once per frame
     void Update()
     {
-        
+        //var scrollView = GetComponent<ScrollRect>();
+        // _scrollView = 
+        // scrollView.verticalNormalizedPosition = 0.0f;
+        // _scrollView.normalizedPosition = new Vector2(0,1);
     }
 }
---
+++ projects/crossword/Unity/Cross/Assets/Scripts/UI/PuzzleGroupRow.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class PuzzleGroupRow : MonoBehaviour
+{
+    public int row;
+    
+    public Image _iconImage;
+    public TextMeshProUGUI _titleText;
+    public TextMeshProUGUI _descriptionText;
+    
+    public delegate void Notify(PuzzleGroupRow group);
+    public event Notify OnRowSelectEvent;
+    
+    public void SetImageFromURL(string url)
+    {
+        // Debug.Log("SET IMAGE: "+url);
+        StartCoroutine(getIconTexture(url));
+
+        _titleText.text = "set the title text";
+        _descriptionText.text = "set the description text";
+    }
+
+    private IEnumerator getIconTexture(string url)
+    {
+        var request = UnityWebRequestTexture.GetTexture(url);
+        yield return request.SendWebRequest();
+        if (request.isHttpError || request.isNetworkError)
+        {
+            Debug.Log("request error: " + request.error);
+        }
+        else
+        {
+            var texture = ((DownloadHandlerTexture) request.downloadHandler).texture;
+            // Debug.Log("request texture: " + texture);
+            Sprite newSprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(.5f, .5f));
+            // Debug.Log("newSprite: " + newSprite);
+            _iconImage.sprite = newSprite;
+        }
+    }
+
+    public void DidSelectRow()
+    {
+        // Debug.Log("RICHIE - DidSelectRow");
+        OnRowSelectEvent?.Invoke(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetImageFromURL("https://images.squarespace-cdn.com/content/v1/5616ac17e4b018d366f57f53/1617124584308-358K4SM1P0Q53DZ6B5RD/LOGO+%282%29.png?format=750w");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
